6c543b795df10b2c7b142acabf31e815fd9e498e
teodordamian2025 <teodordamian@yahoo.com>
2025-07-08 00:21:47 +0300

    Fix export auth din firebaseConfig

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,5 +1,6 @@
 // lib/firebaseConfig.ts
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBcvmm7kHWmQdDX-mMcSnYd05FCIEXMTxc",
@@ -7,9 +8,10 @@ const firebaseConfig = {
   projectId: "unitar-admin",
   storageBucket: "unitar-admin.appspot.com",
   messagingSenderId: "1015955629687",
-  appId: "1:1015955629687:web:947103af6bc62be5b48872",
+  appId: "1:1015955629687:web:947103af6bc62be5b48872"
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export default app;
+export { app, auth };
