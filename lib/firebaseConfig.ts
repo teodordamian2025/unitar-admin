b8d9d7d1ebcae39bece07472cc9d270884f46649
teodordamian2025 <teodordamian@yahoo.com>
2025-07-08 00:12:56 +0300

    Adaugă configurația Firebase pentru autentificare

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,13 +1,13 @@
 // lib/firebaseConfig.ts
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBcvmm7kHWmQdDX-mMcSnYd05FCIEXMTxc',
-  authDomain: 'unitar-admin.firebaseapp.com',
-  projectId: 'unitar-admin',
-  storageBucket: 'unitar-admin.appspot.com',
-  messagingSenderId: '1015955629687',
-  appId: '1:1015955629687:web:947103af6bc62be5b48872'
+  apiKey: "AIzaSyBcvmm7kHWmQdDX-mMcSnYd05FCIEXMTxc",
+  authDomain: "unitar-admin.firebaseapp.com",
+  projectId: "unitar-admin",
+  storageBucket: "unitar-admin.appspot.com",
+  messagingSenderId: "1015955629687",
+  appId: "1:1015955629687:web:947103af6bc62be5b48872",
 };
 
 const app = initializeApp(firebaseConfig);
