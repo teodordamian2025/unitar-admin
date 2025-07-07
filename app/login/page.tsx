aae25e0b2666eaff35528f47afb015c5f706bc5f
teodordamian2025 <teodordamian@yahoo.com>
2025-07-08 00:19:32 +0300

    Corectare import firebaseConfig pentru Vercel

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/lib/firebaseConfig';
+import { auth, app } from '../../lib/firebaseConfig';
 import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
